f905fd87c4a8982f8de595f1d9de21e863d674f5
Celudirosa <cluqued@live.com>
2023-05-29 11:10:52 +0200

    ✨ nav burger menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
 import './navbar.scss';
 
 export default function Navbar() {
+
+  const [ menu , setMenu ] = useState( false )
+
+  const toggleMenu = () => {
+    setMenu( !menu )
+  }
+
   return (
     <>
-      <nav className='navbar'>
-        <ul>
-          <li><a href='#section-about'>ABOUT</a></li>
-          <li><a href='#section-projects'>PROJECTS</a></li>
-          <li><a href='#section-contact'>CONTACT</a></li>
-        </ul>
-      </nav>
+      <header className='header-nav'>
+
+        <h1 className='h1-nav'>
+          <a className='a-nav' href='#section-home'>C</a>
+        </h1>
+
+        <button onClick={ toggleMenu } className='nav-button'>
+          <a className='nav-burger'>🍔</a>
+        </button>
+
+        <nav className={ `main-nav ${ menu ? 'isActive' : '' }` }>
+          <ul className='nav-links'>
+            <li className='link-item'><a className='a-nav' href='#section-about'>ABOUT</a></li>
+            <li className='link-item'><a className='a-nav' href='#section-projects'>PROJECTS</a></li>
+            <li className='link-item'><a className='a-nav' href='#section-contact'>CONTACT</a></li>
+          </ul>
+        </nav>
+
+      </header>
     </>
   )
 }
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -3,9 +3,11 @@ import './home.scss'
 export default function Home() {
   return (
     <>
-      <div className="bg-color-rose-lighter component-container">
-        <h1 className='name'>Hola, soy la Home</h1>
-      </div>
+      <section id='section-home'>
+        <div className="bg-color-rose-lighter component-container">
+          <h1 className='name'>Hola, soy la Home</h1>
+        </div>
+      </section>
     </>
   )
 }
