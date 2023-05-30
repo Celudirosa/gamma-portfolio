82cac7fa8912c68e02aee5f477af3e872036ed8a
Celudirosa <cluqued@live.com>
2023-05-30 16:53:22 +0200

    📝 about & contact info

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -4,7 +4,15 @@ export default function About() {
   return (
     <>
       <section className="bg-color-violet-lighter component-container" id='section-about'>
-        <h1>About</h1>
+        <h1>About Me 🙆🏻‍♀️</h1>
+        <div>
+          <p>Hello! I'm Celia, a multi-talented professional specializing in graphic design, UX/UI design, and front-end web development. Originally from Mérida, I currently reside in Valencia.</p>
+          <p>With a strong background in graphic design, I excel at creating visually appealing designs that effectively communicate messages.</p>
+          <p>Additionally, my expertise in UX/UI design allows me to craft intuitive and seamless user experiences through research and prototyping.</p>
+          <p>Furthermore, I possess in-depth knowledge of front-end web development, transforming design concepts into fully functional websites using HTML, CSS, and JavaScript.</p>
+          <p>I am passionate about continuously learning and growing in my field. If you're looking for a dedicated designer and developer to bring your vision to life, </p>
+        </div>
+        <p>...I would love to collaborate with you! 🔥</p>
       </section>
     </>
   )
--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,7 +4,11 @@ export default function Contact() {
   return (
     <>
       <section className='bg-color-buff component-container' id='section-contact'>
-        <h1>Contact</h1>
+        <h1>Let's get in touch and work on something awesome together! 🎉</h1>
+        <a href="mailto:cluqued@live.com">✉️ cluqued@live.com</a>
+        <a href="">Linkedin</a>
+        <a href="">GitHub</a>
+        <a href="">Instagram</a>
       </section>
     </>
   )
