fa7a3cca447234195568bff56869034f22229d1e
Celudirosa <cluqued@live.com>
2023-05-30 17:54:46 +0200

    🐛 fix title.js

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,19 @@ import './scss/global.scss'
 
 function App() {
 
+    // CHANGE PAGE TITLE
+  // Detect when the user changes tabs
+  document.addEventListener("visibilitychange", function() {
+    if (document.hidden) {
+      // The user is in another tab
+      document.title = "I miss u 🥲"; // Change the tab title
+    } else {
+      // The user returned to the current tab
+      document.title = "Celudirosa"; // Restore the original title
+    }
+  });
+
+
   return (
     <>
       <main>
