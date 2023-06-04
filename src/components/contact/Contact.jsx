b153e3eea6456cee1ade4e0e96893fe74379db3e
Celudirosa <cluqued@live.com>
2023-06-04 17:32:35 +0200

    ✅ svg icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,14 +1,17 @@
 import './contact.scss';
+import { AiFillLinkedin } from 'react-icons/ai';
+import { FaGithubSquare } from 'react-icons/fa';
+import { FaInstagramSquare } from 'react-icons/fa';
 
 export default function Contact() {
   return (
     <>
       <section className='bg-color-buff component-container' id='section-contact'>
         <h1>Let's get in touch and work on something awesome together! 🎉</h1>
-        <a href="mailto:cluqued@live.com">✉️ cluqued@live.com</a>
-        <a href="">Linkedin</a>
-        <a href="">GitHub</a>
-        <a href="">Instagram</a>
+        <a href="mailto:cluqued@live.com" target="_blank">✉️ cluqued@live.com</a>
+        <a href="https://www.linkedin.com/in/celia-luque-d%C3%ADaz-837a99130/" target="_blank"><AiFillLinkedin /> Linkedin</a>
+        <a href="https://github.com/Celudirosa" target="_blank"><FaGithubSquare /> GitHub</a>
+        <a href="https://www.instagram.com/celudirosa.dsgn/" target="_blank"><FaInstagramSquare /> Instagram</a>
       </section>
     </>
   )
