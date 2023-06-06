400741ade09bf2c5ebafb5ddc85f4a9b1fa29d6c
Celudirosa <cluqued@live.com>
2023-06-06 19:45:39 +0200

    🐛 fix svgs name & trying some resolution height

--- src/components/name/Name.jsx
+++ src/components/name/Name.jsx
@@ -13,8 +13,7 @@ export default function Home() {
           <div className='center'>
             <p className='text text-left font-weight-bold'>Hi! 👋🏻 I'm</p>
               <div className='text-center'>
-                <h1 className='hidden'>Celia</h1>
-                <div className='name-svgs text-color-buff-dark'>
+                <div className='text-color-buff-dark'>
                   <SvgC />
                   <SvgE />
                   <SvgL />
