206a39cc4b5625da1bc81b50c7919d1092df59ed
Celudirosa <cluqued@live.com>
2023-06-08 21:11:09 +0200

    🚧 projects.json error 404

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,18 +1,37 @@
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import projectsData from './projects.json';
 import './projects.scss';
 import Project from '../../views/project/Project';
 
 export default function Projects() {
+  const navigate = useNavigate();
+
+  const handleProjectClick = (id) => {
+    navigate(`/project/${id}`);
+  };
+
   return (
     <>
       <section className='bg-color-blue-light component-container' id='section-projects'>
         <div>
           <h1>Projects</h1>
           <div>
-            <Link to='/project'>Project 1</Link>
-          </div>
+          {projectsData.map((project) => (
+            <div key={project.id}>
+              <Link to={`/project/${project.id}`} onClick={() => handleProjectClick(project.id)}>
+                {project.title}
+              </Link>
+            </div>
+          ))}
+        </div>
           <Routes>
-            <Route path='/project' element={<Project />} />
+          {projectsData.map((project) => (
+            <Route
+              key={project.id}
+              path={`/project/${project.id}`}
+              element={<Project project={project} />}
+            />
+          ))}
           </Routes>
         </div>
       </section>
--- src/views/project/Project.jsx
+++ src/views/project/Project.jsx
@@ -1,10 +1,17 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
 import './project.scss';
 
-export default function Project() {
+export default function Project({ project }) {
+  const { id } = useParams();
+
+  const selectedProject = project.find((p) => p.id === id);
+  
   return (
     <>
       <section>
-        <h1>This is one project</h1>
+        <h1>{project.title}</h1>
+        <p>{project.description}</p>
       </section>
     </>
   )
