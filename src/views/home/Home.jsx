02822b439f5400c1cf3ed01de594ad9f195d4b76
Celudirosa <cluqued@live.com>
2023-06-10 22:15:17 +0200

    ✨ sticky page

--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -3,15 +3,26 @@ import Name from '../../components/name/Name';
 import About from '../../components/about/About'
 import Projects from '../../components/projects/Projects';
 import Contact from '../../components/contact/Contact';
+import './home.scss';
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <Name />
-      <About />
-      <Projects />
-      <Contact />
+      <div className="content-home">
+        <Navbar />
+        <div className="sticky-component">
+          <Name />
+        </div>
+        <div className="sticky-component">
+          <About />
+        </div>
+        <div className="sticky-component">
+          <Projects />
+        </div>
+        <div className="sticky-component">
+        <Contact />
+        </div>
+      </div>
     </>
   )
 }
