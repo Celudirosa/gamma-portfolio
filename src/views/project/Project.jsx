79563c983b63c4f14a62a4870e0c69eecf137d65
Celudirosa <cluqued@live.com>
2023-06-13 18:14:10 +0200

    ✨ paragraph break

--- src/views/project/Project.jsx
+++ src/views/project/Project.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import Navbar from '../../components/navbar/Navbar';
+
 import './project.scss';
 
 export default function Project({ projects }) {
@@ -9,12 +9,21 @@ export default function Project({ projects }) {
 	console.log(projects);
 	const selectedProject = projects.find((p) => p.id === id);
 
+	// paragraph break
+	const lines = selectedProject.description.split('\n');
+
 	return (
 		<>
 			<section className="single-project bg-color-buff">
 				<h1>{selectedProject.title}</h1>
 				<img src={selectedProject.image} className="image" alt="" />
-				<p>{selectedProject.description}</p>
+
+				{lines.map((line, index) => (
+					<p key={index} className='single-project-description'>
+						{line}
+					</p>
+				))}
+
 				<a href={selectedProject.links.demo} target="_blank">Demo</a>
 				<a href={selectedProject.links.code} target="_blank">Code</a>
 				<a href="/#section-projects" className="back-button">Back to Projects</a>
